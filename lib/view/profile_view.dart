afe38f55597f11661f6bd540a539bc3cded3a45c
Nicky <n.houter@st.hanze.nl>
2025-01-15 23:25:23 +0100

    added logout button to profile view to be able to test token functionality

--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,15 +1,37 @@
+import 'package:automaat_app/view/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../locator.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  Profile({super.key});
+
+  final secureStorage = locator<FlutterSecureStorage>();
 
 
+  void logOut(context) async {
+    await secureStorage.delete(key: 'token');
+
+    if (context.mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Login()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile"),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(16.0),
+          ),
+            onPressed: () => logOut(context),
+            child: Text("Uitloggen"),
+        ),
+      ),
     );
   }
 }
