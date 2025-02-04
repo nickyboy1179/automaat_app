7926e6aca764d07359d739118399927c18b2480e
Nicky Houter <n.houter@st.hanze.nl>
2025-02-05 00:32:42 +0100

    Ios build configuration changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import flutter_local_notifications
   ) -> Bool {
     //flutter notifications
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-    GeneratedPluginRegistrant.register(with registry)}
+        GeneratedPluginRegistrant.register(with: registry)}
 
     GeneratedPluginRegistrant.register(with: self)
     //flutter notifications
