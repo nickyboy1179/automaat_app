eb498b710c9d7be5022cf7f083d3dcef1812bf71
Nicky <n.houter@st.hanze.nl>
2025-02-06 02:42:53 +0100

    profile view

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,12 +1,20 @@
-import '../service/locator.dart';
-import '../model/rest_model/about_me_model.dart';
-import '../model/retrofit/rest_client.dart';
+import 'package:automaat_app/database/database.dart';
+import 'package:automaat_app/model/rest_model/customer_model.dart';
+import 'package:automaat_app/service/locator.dart';
+import 'package:automaat_app/model/rest_model/about_me_model.dart';
+import 'package:automaat_app/model/retrofit/rest_client.dart';
 
 class ProfileController {
-  final restClient = locator<RestClient>();
+  final _restClient = locator<RestClient>();
+  final _database = locator<AppDatabase>();
 
   Future<AboutMe> getUserInfo() async {
-    AboutMe user = await restClient.getUserInfo();
+    AboutMe user = await _restClient.getUserInfo();
     return user;
+  }
+
+  Future<Customer> getUserInfoLocal() async {
+    List<Customer> customers = await _database.customerDao.getAllCustomers();
+    return customers[0];
   }
 }
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -1,5 +1,7 @@
 import 'package:automaat_app/controller/profile_controller.dart';
 import 'package:automaat_app/database/database.dart';
+import 'package:automaat_app/model/rest_model/about_me_model.dart';
+import 'package:automaat_app/provider/network_state_provider.dart';
 import 'package:automaat_app/service/notification_service.dart';
 import 'package:automaat_app/view/developer_options_view.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +9,14 @@ import 'package:provider/provider.dart';
 import 'package:automaat_app/component/confirm_button.dart';
 import 'package:automaat_app/provider/auth_provider.dart';
 import 'package:automaat_app/service/locator.dart';
-
+import '';
 
 class ProfileView extends StatelessWidget {
   ProfileView({super.key});
 
   final ProfileController controller = ProfileController();
-  final NotificationService _notificationService = locator<NotificationService>();
+  final NotificationService _notificationService =
+      locator<NotificationService>();
   final AppDatabase _database = locator<AppDatabase>();
 
   void logOut(BuildContext context) async {
@@ -24,39 +27,106 @@ class ProfileView extends StatelessWidget {
     await context.read<AuthProvider>().logout(context);
   }
 
+  Future<dynamic> getUserInfo(BuildContext context) async {
+    final NetworkStateProvider network =
+        Provider.of<NetworkStateProvider>(context, listen: false);
+    if (network.isConnected) {
+      return await controller.getUserInfo();
+    } else {
+      return await controller.getUserInfoLocal();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        appBar: AppBar(
+          title: const Text(
+            "Profiel",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                FutureBuilder(
+                    future: getUserInfo(context),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text("Error: ${snapshot.error}"));
+                      } else if (!snapshot.hasData) {
+                        return Center(
+                            child:
+                                Text("Geen gebruikersInformatie beschikbaar"));
+                      } else {
+                        return _infoCard(context, snapshot.data);
+                      }
+                    }),
+                SizedBox(height: MediaQuery.sizeOf(context).height * 0.4,),
+                ConfirmButton(
+                  text: 'Uitloggen',
+                  color: Colors.red,
+                  onColor: Colors.white,
+                  onPressed: () {
+                    logOut(context);
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                ConfirmButton(
+                  text: 'Developer opties',
+                  color: Theme.of(context).colorScheme.primary,
+                  onColor: Theme.of(context).colorScheme.onPrimary,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DeveloperOptionsView(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            )));
+  }
+
+  Widget _infoCard(BuildContext context, dynamic userInfo) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12)
+      ),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        width: MediaQuery.sizeOf(context).width,
+        decoration: BoxDecoration(
+          color: colorScheme.surface,
+          borderRadius: BorderRadius.circular(12),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            ConfirmButton(
-              text: 'Uitloggen',
-              color: Theme.of(context).colorScheme.primary,
-              onColor: Theme.of(context).colorScheme.onPrimary,
-              onPressed: () {
-                logOut(context);
-              },
-            ),
-            SizedBox(height: 16,),
-            ConfirmButton(
-              text: 'Developer opties',
-              color: Theme.of(context).colorScheme.primary,
-              onColor: Theme.of(context).colorScheme.onPrimary,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DeveloperOptionsView(),
-                  ),
-                );
-              },
+            Icon(Icons.person, size: 50, color: colorScheme.primary),
+            SizedBox(height: 8),
+            Text(
+              '${userInfo.firstName} ${userInfo.lastName}',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: colorScheme.primary,
+              ),
             ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
